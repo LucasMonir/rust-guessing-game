52c1806af29e012312c5718b6c55670c58e32154
Lucas <lucasmonir@hotmail.com>
2022-05-23 22:32:26 -0300

    Completed the tutorial.

--- src/main.rs
+++ src/main.rs
@@ -5,23 +5,30 @@ fn main() {
     println!("Guess the number!");
 
     let secret_number = rand::thread_rng().gen_range(1..101);
+    loop {
+        println!("Please input your guess: ");
+        let mut guess = String::new();
 
-    println!("Please input your guess: ");
-    let mut guess = String::new();
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line >:(");
+        
+   
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line >:(");
+        println!("You guessed: {}", guess);
+        
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue, 
+        };
 
-    println!("You guessed: {}", guess);
-
-    let guess: u32 = guess.trim().parse().expect("Please inform a number");
-
-    match guess.cmp(&secret_number) {
-        Ordering::Less => println!("Too small"),
-        Ordering::Greater => println!("Too big"),
-        Ordering::Equal => println!("Perfect!"),
-    }
-
-    println!("The number: {}", secret_number);
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small"),
+            Ordering::Greater => println!("Too big"),
+            Ordering::Equal => {
+                println!("Perfect!");
+                break;
+            }
+        }
+    }   
 }
